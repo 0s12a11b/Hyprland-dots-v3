39293c092fce3a0c091f48505d5b04010c63e21e
Gleb Yadykin <0s12a11b@gmail.com>
2024-12-30 11:54:49 +0300

    Add files via upload

--- waybar/scripts/telegram.sh
+++ waybar/scripts/telegram.sh
@@ -1,2 +1,18 @@
-killall telegram-desktop
-telegram-desktop
+#!/bin/bash
+
+# Проверяем, запущен ли процесс Telegram Desktop
+if pgrep -x "telegram-desktop" > /dev/null; then
+    # Если процесс запущен, проверяем наличие окна
+    WIN_ID=$(wmctrl -lx | grep "telegram-desktop.Telegram" | awk '{print $1}')
+    if [ -n "$WIN_ID" ]; then
+        # Если окно найдено, перемещаем его на текущий рабочий стол
+        wmctrl -i -R "$WIN_ID"
+    else
+        # Если окно скрыто или свернуто, открываем приложение
+        telegram-desktop &
+    fi
+else
+    # Если процесс не запущен, запускаем приложение
+    telegram-desktop &
+fi
+
